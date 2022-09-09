f7fc7d15b11c53d8bce19ea7935c123379015d88
Yang Zhang <yang@users.noreply.github.com>
2022-09-08 21:26:44 -0700

    Add Plasmic

---
+++ app/routes/mypage.tsx
@@ -0,0 +1,30 @@
+/** @format */
+
+// This page will show up at the route /mypage
+
+import {
+  initPlasmicLoader,
+  PlasmicRootProvider,
+  PlasmicComponent,
+  ComponentRenderData,
+} from "@plasmicapp/loader-react";
+import { PLASMIC } from "../plasmic-init";
+import { useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+
+// Fetch the data needed to render Plasmic pages or components, server-side.
+export const loader = async () => {
+  // You can pass in multiple page paths or component names.
+  const plasmicData = await PLASMIC.fetchComponentData("Homepage");
+  return json(plasmicData);
+};
+
+// Render the page or component from Plasmic.
+export default function MyPage() {
+  const plasmicData = useLoaderData();
+  return (
+    <PlasmicRootProvider loader={PLASMIC} prefetchedData={plasmicData}>
+      <PlasmicComponent component="Homepage" />
+    </PlasmicRootProvider>
+  );
+}
